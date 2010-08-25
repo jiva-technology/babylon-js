8450fb60c836f9d5eb7f063e18473abed8c69643
Jamie Dyer <jamie@kernowsoul.com>
2010-08-25 15:53:38 +0100

    [4719468] feature: added support for callbacks using Strophe's sendIQ

--- spec/javascripts/controller_spec.js
+++ spec/javascripts/controller_spec.js
@@ -1,17 +1,15 @@
 describe("Babylon.Controller", function() {
-  var controller = {};
-  var action = "";
-  var name = "";
+  
+  var controller, xml_view_object;
+  var action  = "test-action";
+  var name    = "test-controller";
 
   beforeEach(function(){
-    action = "test-action";
-    name = "test-controller";
-
-    controller = new Babylon.Controller();
-    controller.name = name;
+    controller        = new Babylon.Controller();
+    controller.name   = name;
     controller[action] = function(){};
-
-    Babylon.Views.add(name, action, function(){ return "default view"; });
+    xml_view_object = xml("iq");
+    Babylon.Views.add(name, action, function(){ return xml_view_object; });
   });
 
   afterEach(function() {
@@ -19,11 +17,6 @@ describe("Babylon.Controller", function() {
   });
 
   describe("perform", function() {
-    beforeEach(function() {
-      controller = new Babylon.Controller();
-      controller.name = name;
-      controller[action] = function(){};
-    });
 
     it("should set the action_name parameter", function() {
       controller.perform(action);
@@ -36,38 +29,30 @@ describe("Babylon.Controller", function() {
       expect(controller.perform_action_set_var).toEqual("xyz");
     });
 
-    it("shouuld render the view attached to the action", function() {
-      expect(controller.perform(action)()).toEqual("default view");
+    it("should render the view attached to the action", function() {
+      expect(controller.perform(action)()).toEqual(xml_view_object);
     });
-  });
+  }); // end describe
 
   describe("render", function() {
-    beforeEach(function(){
-      controller = new Babylon.Controller();
-      controller.name = name;
-      controller[action] = function(){};
-    });
 
     describe("when render has been called once before", function() {
       beforeEach(function() {
         controller.render({view: true});
       });
+      
       it("should return false", function(){
         expect(controller.render()).toEqual(false);
       });
+    }); // end describe
+
+    it("when {nothing: true} is passed in should set this.view to a callable that returns \"\"", function(){
+      controller.render({nothing: true});
+      expect(controller.view({})).toEqual("");
     });
 
-    describe("when {nothing: true} is passed in", function() {
-      it("should set this.view to a callable that returns \"\"", function(){
-        controller.render({nothing: true});
-        expect(controller.view({})).toEqual("");
-      });
-    });
-
-    describe("when {view: xxx} is passed in", function(){
-      it("should set this.view to that function", function() {
-        expect(controller.render({view: function(l){return "test"; }})()).toEqual("test");
-      });
+    it("when {view: xxx} is passed in should set this.view to that function", function() {
+      expect(controller.render({view: function(l){return "test"; }})()).toEqual("test");
     });
 
     describe("when {action: xxx} is passed in", function(){
@@ -76,17 +61,14 @@ describe("Babylon.Controller", function() {
         Babylon.Views.add(name, "alt-action", function(){ return "abcd"; });
       });
 
-      afterEach(function() {
-        Babylon.Views.clear();
-      });
-
       it("should pass control to action xxx", function() {
         expect(controller.render({action: "alt-action"})()).toEqual("abcd");
       });
-    });
-  });
+    }); // end describe
+  }); // end describe
 
   describe("evaluate", function() {
+    
     it("should return \"\" if this.view is unset", function(){
       controller.view = null;
       expect(controller.evaluate()).toEqual("");
@@ -97,5 +79,25 @@ describe("Babylon.Controller", function() {
       controller.bound_var = "tada";
       expect(controller.evaluate()).toEqual("tada");
     });
-  });
-});
+  }); // end describe
+
+  describe("render_with_callbacks", function() {
+    
+    beforeEach(function() {
+      Babylon.Runner.connection = {};
+      Babylon.Runner.connection.connection = {};
+      var con_mock = new Mock(Babylon.Runner.connection.connection);
+      
+      controller.action_name = action;
+      
+      // stop auto rendering
+      controller.render({ nothing: true });
+    }); // end before
+    
+    it("should call the sendIQ method of Strophe", function() {
+      // TODO this matcher should match against the xml_view_object but jsmocha is not correctly matching the objects
+      Babylon.Runner.connection.connection.expects('sendIQ').passing(Match.an_object, Match.a_function, Match.a_function, 10000);
+      controller.render_with_callbacks({ data: 'blah', success: function(){}, fail: function(){} });
+    }); // end it
+  }); // end describe
+}); // end describe
